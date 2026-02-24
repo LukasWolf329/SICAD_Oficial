e9739fb453f24c23f1a8fe510b44cb638dabbac4
LorenzoBertozzi <lorenzobertozzi847@gmail.com>
2026-02-24 14:09:34 -0300

    ajuste estetico aside bar

--- app/(tabs)/(painel)/settings/page.tsx
+++ app/(tabs)/(painel)/settings/page.tsx
@@ -12,26 +12,26 @@ export default function Settings() {
 
   return (
     <ScrollView className="flex-1 dark:bg-[#121212] ">
-        <View className="bg-white dark:bg-[#242424] p-4 pb-20 items-center justify-center rounded-md">
+        <View className="bg-white dark:bg-[#242424]  p-4 pb-20 items-center justify-center rounded-md">
             <View className="w-4/12">
                 {/* Informações Básicas */}
-                <View className="border-b-2 border-slate-300 pb-4 mb-4">
+                <View className="border-b-2 border-slate-300 pb-4 mb-4 ">
                     <View>
-                    <Text className="font-bold text-2xl">Informações Basicas</Text>
-                    <Text>Dê um nome ao seu evento, faça ele se destacar e o torne unico</Text>
-                    </View> <View className="mt-4">
-                        <Text>Titulo do Evento</Text>
+                    <Text className="dark:color-white font-bold text-2xl ">Informações Basicas</Text>
+                    <Text className='dark:color-white '>Dê um nome ao seu evento, faça ele se destacar e o torne unico</Text>
+                    </View> <View className="mt-4 ">
+                        <Text className='dark:color-white '>Titulo do Evento</Text>
                         <TextInput className="mb-2 w-full bg-transparent border border-slate-400 rounded-lg px-2 py-1 color-slate-500 dark:color-white"/>
-                        <Text>Link de Acesso</Text>
+                        <Text className='dark:color-white '>Link de Acesso</Text>
                         <TextInput className="mb-2 w-full bg-transparent border border-slate-400 rounded-lg px-2 py-1 color-slate-500 dark:color-white"/>
                         <View className="flex-row gap-8">
                             <View className="w-6/12">
-                                <Text>Idiona do Evento</Text>
+                                <Text className='dark:color-white '>Idiona do Evento</Text>
                                 <TextInput className="w-full bg-transparent border border-slate-400 rounded-lg px-2 py-1 color-slate-500 dark:color-white"/>
-                                <Text>As mensagens da plataforma serão enviado neste idioma</Text>
+                                <Text className='dark:color-white '>As mensagens da plataforma serão enviado neste idioma</Text>
                             </View>
                             <View className="w-5/12">
-                                <Text>Carga Horario Total</Text>
+                                <Text className='dark:color-white '>Carga Horario Total</Text>
                                 <TextInput className="mb-2 w-full bg-transparent border border-slate-400 rounded-lg px-2 py-1 color-slate-500 dark:color-white"/>
                             </View>
                         </View>
@@ -42,8 +42,8 @@ export default function Settings() {
 
                 {/* Local */}
                 <View className="border-b-2 border-slate-300 pb-4 mb-4">
-                    <Text className="font-bold text-2xl">Local</Text>
-                    <Text>Ajude as pessoas a descobrirem seu evento e informe os participantes onde comparecer</Text>
+                    <Text className="font-bold text-2xl dark:color-white">Local</Text>
+                    <Text className='dark:color-white '>Ajude as pessoas a descobrirem seu evento e informe os participantes onde comparecer</Text>
                     
                     <View className="mt-4 flex-row gap-4">
                         {options.map((option) => (
@@ -53,16 +53,16 @@ export default function Settings() {
                             onPress={() => setLocal(option.value)}
                             >
                             <View
-                                className={`w-5 h-5 mr-2 rounded-full border-2 ${
+                                className={`w-5 h-5 mr-2 rounded-full border-2 dark:color-white ${
                                 local === option.value ? 'border-blue-500 bg-blue-500' : 'border-gray-400'
                                 }`}
                             />
-                            <Text>{option.label}</Text>
+                            <Text className='dark:color-white'>{option.label}</Text>
                             </Pressable>
                         ))}
                     </View>
                     <View className="mt-4">
-                        <Text>Local</Text>
+                        <Text className='dark:color-white '>Local</Text>
                         <TextInput className="mb-2 w-full bg-transparent border border-slate-400 rounded-lg px-2 py-1 color-slate-500 dark:color-white"/>
                     </View>
                 </View>
